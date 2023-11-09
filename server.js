54cd09d096d435083fba4b46ea980a9db5a378f3
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-11-09 18:26:52 +0100

    Project setup done

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict"
+
+import express from 'express'
+import 'dotenv/config'
+
+const server = express()
+const PORT = process.env.PORT
+
+server.use(express.json())
+server.use((req, res, next) => {
+  console.log(req.method, req.url, req.body)
+  console.log(res.method, res.url, res.body)
+  console.log("===== MW END =====")
+  next()
+})
+
+server.listen(PORT, ()=> console.log(`Running Btc on ${PORT}`))
